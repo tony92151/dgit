c71fd09997f5e058be8a89dfa2c685e9ec458b8d
tony92151 <tony92151@gmail.com>
2021-11-11 10:55:34 +0800

    fix checkout

--- dgit/commands/dgit_checkout.py
+++ dgit/commands/dgit_checkout.py
@@ -4,6 +4,20 @@ from git import Repo
 from .utils import command_run, DGIT_DATA_FILE
 
 
+def dgit_checkout(dvc_path, repo: Repo, selected_tag,args, unknowargs: list):
+    # print("")
+    # repo.pull()
+    # com = "git pull".format(dvc_path)
+    # command_run(command=com)
+    repo.git.checkout(selected_tag)
+
+    if args.without_data:
+        pass
+    else:
+        com = "dvc --cd {} pull {}".format(dvc_path, DGIT_DATA_FILE)
+        command_run(command=com)
+
+
 class CMD_init:
     def __init__(self, subparsers):
         self.command_help = "checkout to different version."
@@ -19,6 +33,12 @@ class CMD_init:
         self.parser.add_argument(
             '--list-tags',
             help='list all tags and select.',
+            action='store_true'
+        )
+
+        self.parser.add_argument(
+            '--without-data',
+            help='pull {} only'.format(DGIT_DATA_FILE),
             action='store_true'
         )
 
@@ -40,5 +60,10 @@ class CMD_init:
             selected_tag = str(repo.tags[int(input("? "))])
         else:
             selected_tag = unknownargs[0]
-        repo.git.checkout(selected_tag)
+
+        dgit_checkout(dvc_path,
+                      repo,
+                      selected_tag,
+                      args,
+                      unknownargs)
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="dgit",
-    version="1.1",
+    version="0.5.1",
     author="tonyguo",
     author_email="tony92151@gmail.com",
     url="https://github.com/tony92151/dgit",
